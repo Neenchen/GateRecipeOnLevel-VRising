21742563fb3b1bd0dc29f6f9d538206f7c51bf50
Neenchen <pinkkurdi@hotmail.com>
2026-06-01 14:13:41 +0200

    New modification

--- obj/Debug/net6.0/LevelRecipeGate.AssemblyInfo.cs
+++ obj/Debug/net6.0/LevelRecipeGate.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LevelRecipeGate")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+da83d1911b3c877edec3d1d21b5267eca3165838")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7541327046635173a3dc1a31abe93a26fa90f50d")]
 [assembly: System.Reflection.AssemblyProductAttribute("LevelRecipeGate")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LevelRecipeGate")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
